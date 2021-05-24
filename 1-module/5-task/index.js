2c3f8b0b464e6f33b4ba5b23682cf879cafadcef
Sergey <bivenmira1990@gmail.com>
2021-05-24 22:46:26 +0300

    change_5_task

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,4 +1,4 @@
 function truncate(str, maxlength) {
-  return str.length > maxlength ? str.slice(0, maxlength - 1) + "…":str;
+  return str.length > maxlength ? str.slice(0, maxlength - 1) + "…" : str;
 }
 truncate( "Seegey is good man", 10 )

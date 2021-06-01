bcabd7316ccfeb3be8b625bf2e6af7e88aac7620
Sergey <bivenmira1990@gmail.com>
2021-06-01 12:51:37 +0300

    3_module_4_tasks

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,5 +1,5 @@
 function showSalary(users, age) {
-  let useAge = users.filter((item) => item.age <= age);
-  let str = useAge.map((item) => item.name + ', ' + item.balance).join('\n');
+  let userAge = users.filter((item) => item.age <= age);
+  let str = userAge.map((item) => item.name + ', ' + item.balance).join('\n');
   return str;
 }

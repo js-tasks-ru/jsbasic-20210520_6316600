b833b54c14019f3dda50994dff91b927ebaace47
Sergey <bivenmira1990@gmail.com>
2021-06-02 13:21:00 +0300

    3_module_5_task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(",").join(" ").split(" ");
+  let newArr = [];
+  arr.forEach((item) => {
+    if (parseInt(item)) {
+      newArr.push(parseFloat(item));
+    }
+  });
+  let result = {
+    min: Math.min(...newArr),
+    max: Math.max(...newArr),
+  };
+  return result;
 }

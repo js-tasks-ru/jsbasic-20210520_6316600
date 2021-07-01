db9f915523b689b489653dfce8ed43d19b89a4c4
Biven160690 <bivenmira1990@gmail.com>
2021-07-01 10:51:26 +0300

    7_module_2_task

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,6 +1,45 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class Modal {
   constructor() {
+    this.modal = document.createElement("div");
+    this.modal.classList.add("modal");
+    this.modal.insertAdjacentHTML(
+      "beforeend",
+      `<div class="modal__overlay"></div>
+    <div class="modal__inner">
+      <div class="modal__header">
+        <!--Кнопка закрытия модального окна-->
+        <button type="button" class="modal__close">
+          <img src="/jsbasic-20210520_6316600/assets/images/icons/cross-icon.svg" alt="close-icon" />
+        </button>
+        <h3 class="modal__title">
+        </h3>
+      </div>
+      <div class="modal__body">
+      </div>`
+    );
+    document.addEventListener("keydown", (event) => {
+      event.code === "Escape" && this.close();
+    });
+    this.modal.querySelector(".modal__close").addEventListener("click", () => {
+      this.close();
+    });
+  }
+  open() {
+    document.body.classList.add("is-modal-open");
+    document.body.append(this.modal);
+  }
+  setTitle(text) {
+    this.text = text;
+    this.modal.querySelector(".modal__title").innerHTML = this.text;
+  }
+  setBody(node) {
+    this.node = node;
+    this.modal.querySelector(".modal__body").append(this.node);
+  }
+  close() {
+    this.modal.remove();
+    document.querySelector("body").classList.remove("is-modal-open");
   }
 }
